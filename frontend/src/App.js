9211f55094eb034a23bd7ed7e1639dda634a9acb
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2024-03-22 14:39:51 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import FetchDataComponent from './FetchDataComponent';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>MongoDB Frontend</h1>
+      <FetchDataComponent />
+    </div>
+  );
+};
+
+export default App;
